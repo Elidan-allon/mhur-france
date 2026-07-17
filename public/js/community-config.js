40d1f1be16da281d9e64a8de562c88dfb850be74
Elidan-allon <eallon01@gmail.com>
2026-07-17 03:58:45 +0300

    Update community-config.js

--- public/js/community-config.js
+++ public/js/community-config.js
@@ -1,4 +1,4 @@
 window.MHUR_COMMUNITY_CONFIG = {
-  supabaseUrl: "https://ypaivlgghfqbddqridyq.supabase.co",
-  supabaseKey: "sb_publishable_K4k1_ToJ_HpN3L7RlsRh1w_w37r4b1H"
+  supabaseUrl:"https://ypaivlgghfqbddqridyq.supabase.co",
+  supabaseKey:"sb_publishable_K4k1_ToJ_HpN3L7RlsRh1w_w37r4b1H"
 };
