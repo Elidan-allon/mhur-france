2a35d3ee9384a23c5d3627de1e9edc9abc642a8f
Elidan-allon <eallon01@gmail.com>
2026-07-20 12:35:00 +0300

    Final!!!!!

--- public/js/community-config.js
+++
@@ -1,4 +0,0 @@
-window.MHUR_COMMUNITY_CONFIG = {
-  supabaseUrl:"https://erjekxgufcfpdziuowtd.supabase.co",
-  supabaseKey:"sb_publishable_6A4yx5BI2Gyebc-PYwtpvA_izT3jQge"
-};
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const RELEASE='406';
+const RELEASE='409';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
   const keys=await caches.keys();
