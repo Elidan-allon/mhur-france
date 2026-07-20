2879936f6c1bf05b9f20d0d8b1ba01ef045f0d22
Elidan-allon <eallon01@gmail.com>
2026-07-20 11:00:36 +0300

    jai regles les installation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const RELEASE='402';
+const RELEASE='406';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
   const keys=await caches.keys();
